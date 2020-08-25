cb6dc2aa493b49aa4ed8ed203d4ae0471fc452bf
Tamara Salvatori <tamara.salvatori@thoughtworks.com>
2020-08-25 09:37:20 -0300

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by LA Tamara Salvatori on 25/08/20.
+//  Copyright © 2020 Tamara Salvatori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
